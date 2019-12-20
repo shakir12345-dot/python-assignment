b7e475ed7cebf8b46c9c8ff5d78e7269b2e37360
shakir hussain <ah865420@gmail.com>
2019-12-20 22:43:25 +0500

    python programming

---
+++ Assignment 6.py
@@ -0,0 +1,39 @@
+Question 1:
+     define object oriented programming?
+ans:
+     object oriented programming is based on objects which contain data in form of field and code.
+
+
+
+Question 2:
+     list down the benefits of oop?
+ans:
+     its provides a clear modular structure for pragramming which makes it good for defindingabstract datatypes
+     in which implementation deatail are hidden.
+
+
+Question 3:
+     differentiate between function and method?
+ans:
+     the major difference between function and method is that function are defined outside of classes
+     and method are defined inside of and part of class.
+
+Question 4:
+     define the following terms:
+     1.class
+     2.object
+     3.attributes
+     4.behavior
+
+ans:
+    class:
+        we use class for keep related things together.
+
+    object:
+        object is simply a collection of
+
+    attributes:
+        attributes belong to the class its self they will be shared by all the instance.
+
+    behavior:
+        behave is behavior driven development for style.
